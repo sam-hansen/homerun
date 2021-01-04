87daaecd1861b6f8c6c5ec6909363db01def31a6
BLVCKLNGS <hugo.poullet@epitech.eu>
2021-01-04 15:37:46 +0100

    Fixed prettier default formatting

--- packages/create-homerun-app/create-app.ts
+++ packages/create-homerun-app/create-app.ts
@@ -14,27 +14,36 @@ const pipeline = promisify(Stream.pipeline);
  * @param root Root directory
  * @param dependencies Dependencies list
  */
-async function install(root: string, dependencies: string[] | null): Promise<void> {
-	return new Promise((resolve, reject) => {
-		let command: string;
-		let args: string[];
+async function install(
+    root: string,
+    dependencies: string[] | null
+): Promise<void> {
+    return new Promise((resolve, reject) => {
+        let command: string;
+        let args: string[];
 
-		command = "npm";
-		args = (["install", dependencies && "--save", dependencies && "--save-extract", "--loglevel", "error"].filter(Boolean) as string[]).concat(dependencies || []);
+        command = "npm";
+        args = ([
+            "install",
+            dependencies && "--save",
+            dependencies && "--save-extract",
+            "--loglevel",
+            "error",
+        ].filter(Boolean) as string[]).concat(dependencies || []);
 
-		const child = spawn(command, args, {
-			stdio: "inherit",
-			env: { ...process.env },
-		});
+        const child = spawn(command, args, {
+            stdio: "inherit",
+            env: { ...process.env },
+        });
 
-		child.on("close", (code) => {
-			if (code !== 0) {
-				reject({ command: `${command} ${args.join(" ")}` });
-				return;
-			}
-			resolve();
-		});
-	});
+        child.on("close", (code) => {
+            if (code !== 0) {
+                reject({ command: `${command} ${args.join(" ")}` });
+                return;
+            }
+            resolve();
+        });
+    });
 }
 
 /**
@@ -42,47 +51,70 @@ async function install(root: string, dependencies: string[] | null): Promise<voi
  * @param root Root directory
  */
 async function download(root: string): Promise<void> {
-	return pipeline(got.stream("https://codeload.github.com/twickd/homerun/tar.gz/master"), tar.extract({ cwd: root, strip: 3 }, ["homerun-master/packages/homerun-core"]));
+    return pipeline(
+        got.stream("https://codeload.github.com/twickd/homerun/tar.gz/master"),
+        tar.extract({ cwd: root, strip: 3 }, [
+            "homerun-master/packages/homerun-core",
+        ])
+    );
 }
 
-export async function createApp({ appPath }: { appPath: string }): Promise<void> {
-	const root: string = path.resolve(appPath);
-	const appName: string = path.basename(root);
+export async function createApp({
+    appPath,
+}: {
+    appPath: string;
+}): Promise<void> {
+    const root: string = path.resolve(appPath);
+    const appName: string = path.basename(root);
 
-	try {
-		fs.accessSync(path.dirname(root), fs.constants.W_OK);
-	} catch (e) {
-		console.error(`The application path is not writeable, please check folder permissions and try again`);
-		process.exit(1);
-	}
+    try {
+        fs.accessSync(path.dirname(root), fs.constants.W_OK);
+    } catch (e) {
+        console.error(
+            `The application path is not writeable, please check folder permissions and try again`
+        );
+        process.exit(1);
+    }
 
-	console.log(`Creating Homerun app in ${chalk.green(root)}`);
-	try {
-		fs.mkdirSync(root);
-	} catch (e) {
-		console.error(`${chalk.red(chalk.bold("Error !"))} Could not create application path. Exiting`);
-		process.exit(1);
-	}
-	process.chdir(root);
+    console.log(`Creating Homerun app in ${chalk.green(root)}`);
+    try {
+        fs.mkdirSync(root);
+    } catch (e) {
+        console.error(
+            `${chalk.red(
+                chalk.bold("Error !")
+            )} Could not create application path. Exiting`
+        );
+        process.exit(1);
+    }
+    process.chdir(root);
 
-	try {
-		await retry(() => download(root), { retries: 3 });
-	} catch (reason) {
-		throw reason;
-	}
+    try {
+        await retry(() => download(root), { retries: 3 });
+    } catch (reason) {
+        throw reason;
+    }
 
-	console.log(chalk.dim("Installing dependencies... Might take few minutes"));
-	await install(root, null);
+    console.log(chalk.dim("Installing dependencies... Might take few minutes"));
+    await install(root, null);
 
-	console.log(`${chalk.green(chalk.bold("Success !"))} Created ${appName} at ${appPath}`);
-	console.log("Here is a list of command you might want to use:");
-	console.log("\n");
-	console.log(`    ${chalk.cyan(chalk.bold("npm"))} ${chalk.dim("run")} dev`);
-	console.log(`    Start the development server with watcher`);
-	console.log("\n");
-	console.log(`    ${chalk.cyan(chalk.bold("npm"))} ${chalk.dim("run")} build`);
-	console.log(`    Build the app for production`);
-	console.log("\n");
-	console.log(`    ${chalk.cyan(chalk.bold("npm"))} ${chalk.dim("run")} start`);
-	console.log(`    Run the built app in production mode`);
+    console.log(
+        `${chalk.green(
+            chalk.bold("Success !")
+        )} Created ${appName} at ${appPath}`
+    );
+    console.log("Here is a list of command you might want to use:");
+    console.log("\n");
+    console.log(`    ${chalk.cyan(chalk.bold("npm"))} ${chalk.dim("run")} dev`);
+    console.log(`    Start the development server with watcher`);
+    console.log("\n");
+    console.log(
+        `    ${chalk.cyan(chalk.bold("npm"))} ${chalk.dim("run")} build`
+    );
+    console.log(`    Build the app for production`);
+    console.log("\n");
+    console.log(
+        `    ${chalk.cyan(chalk.bold("npm"))} ${chalk.dim("run")} start`
+    );
+    console.log(`    Run the built app in production mode`);
 }
--- packages/create-homerun-app/index.ts
+++ packages/create-homerun-app/index.ts
@@ -9,53 +9,53 @@ import pJson from "./package.json";
 let projectPath: string = "";
 
 const program = new Commander.Command(pJson.name)
-	.version(pJson.version)
-	.arguments("<app-directory>")
-	.usage("<app-directory>")
-	.action((name) => {
-		projectPath = name;
-	})
-	.parse(process.argv);
+    .version(pJson.version)
+    .arguments("<app-directory>")
+    .usage("<app-directory>")
+    .action((name) => {
+        projectPath = name;
+    })
+    .parse(process.argv);
 
 async function run(): Promise<void> {
-	if (typeof projectPath === "string") {
-		projectPath = projectPath.trim();
-	}
+    if (typeof projectPath === "string") {
+        projectPath = projectPath.trim();
+    }
 
-	if (!projectPath) {
-		const res = await prompts({
-			type: "text",
-			name: "path",
-			message: "What is your project named?",
-			initial: "my-homerun-app",
-		});
+    if (!projectPath) {
+        const res = await prompts({
+            type: "text",
+            name: "path",
+            message: "What is your project named?",
+            initial: "my-homerun-app",
+        });
 
-		if (typeof res.path === "string") {
-			projectPath = res.path.trim();
-		}
-	}
+        if (typeof res.path === "string") {
+            projectPath = res.path.trim();
+        }
+    }
 
-	const resolvedProjectPath = path.resolve(projectPath);
-	const projectName = path.basename(resolvedProjectPath);
+    const resolvedProjectPath = path.resolve(projectPath);
+    const projectName = path.basename(resolvedProjectPath);
 
-	try {
-		await createApp({
-			appPath: resolvedProjectPath,
-		});
-	} catch (reason) {
-		throw reason;
-	}
+    try {
+        await createApp({
+            appPath: resolvedProjectPath,
+        });
+    } catch (reason) {
+        throw reason;
+    }
 }
 
 run()
-	.then()
-	.catch(async (reason) => {
-		console.log("Aborting installation");
-		if (reason.command) {
-			console.error(`\t${chalk.cyan(reason.command)} has failed`);
-		} else {
-			console.error(`${chalk.red("Unknown error")}:\n${reason}`);
-		}
+    .then()
+    .catch(async (reason) => {
+        console.log("Aborting installation");
+        if (reason.command) {
+            console.error(`\t${chalk.cyan(reason.command)} has failed`);
+        } else {
+            console.error(`${chalk.red("Unknown error")}:\n${reason}`);
+        }
 
-		process.exit(1);
-	});
+        process.exit(1);
+    });
