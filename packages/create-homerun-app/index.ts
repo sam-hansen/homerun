fa3ed531ede1a2d78682af4f972d0796fde2e242
BLVCKLNGS <hugo.poullet@epitech.eu>
2021-01-04 15:22:35 +0100

    create-homerun-app

---
+++ packages/create-homerun-app/index.ts
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+import path from "path";
+import prompts from "prompts";
+import { createApp } from "./create-app";
+import chalk from "chalk";
+
+let projectPath: string = "";
+
+async function run(): Promise<void> {
+	if (typeof projectPath === "string") {
+		projectPath = projectPath.trim();
+	}
+
+	if (!projectPath) {
+		const res = await prompts({
+			type: "text",
+			name: "path",
+			message: "What is your project named?",
+			initial: "my-homerun-app",
+		});
+
+		if (typeof res.path === "string") {
+			projectPath = res.path.trim();
+		}
+	}
+
+	const resolvedProjectPath = path.resolve(projectPath);
+	const projectName = path.basename(resolvedProjectPath);
+
+	try {
+		await createApp({
+			appPath: resolvedProjectPath,
+		});
+	} catch (reason) {
+		throw reason;
+	}
+}
+
+run()
+	.then()
+	.catch(async (reason) => {
+		console.log("Aborting installation");
+		if (reason.command) {
+			console.error(`\t${chalk.cyan(reason.command)} has failed`);
+		} else {
+			console.error(`${chalk.red("Unknown error")}:\n${reason}`);
+		}
+
+		process.exit(1);
+	});
