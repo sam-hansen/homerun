860dd861a04495700cd86f7ce850ba0ebe0639d8
hugovntr <hugo.poullet@epitech.eu>
2021-01-19 12:08:11 +0100

    :fire: Temporarly removed Layout

--- packages/homerun-core/src/components/layout.tsx
+++ packages/homerun-core/src/components/layout.tsx
@@ -1,30 +1,5 @@
 import React from "react";
-function Footer({ config }) {
-    return (
-        <footer className="py-8">
-            <div className="container mx-auto px-4">
-                <Credits config={config} />
-            </div>
-        </footer>
-    );
-}
 
-export function Credits({ config }): JSX.Element {
-    return (
-        <div className="flex flex-col">
-            <a href={config.repo_url} className="anchor">
-                {config.repo_name}
-            </a>
-            <span className="text-xs text-accent-5">Powered by Homerun</span>
-        </div>
-    );
-}
-
-export default function Layout({ children, config = {} }): JSX.Element {
-    return (
-        <div id="__homerun">
-            {children}
-            <Footer config={config} />
-        </div>
-    );
+export default function Layout({ children }): JSX.Element {
+    return <div id="__homerun">{children}</div>;
 }
